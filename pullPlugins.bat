be499893f965ad95137683f8469d8c322f311156
Idan Arye <IdanArye@gmail.com>
2011-10-26 01:14:22 +0200

    Adding vim-unimpaired plugin

--- pullPlugins.bat
+++ pullPlugins.bat
@@ -4,3 +4,4 @@ CALL gitPullPlugin.bat git://github.com/vim-scripts dbext.vim
 CALL gitPullPlugin.bat git://github.com/tpope vim-fugitive
 CALL gitPullPlugin.bat git://repo.or.cz vcscommand .git
 CALL gitPullPlugin.bat git://github.com/gabemc powershell-vim
+CALL gitPullPlugin.bat git://github.com/tpope vim-unimpaired.git
