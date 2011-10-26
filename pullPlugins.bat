87e257ddb8904dc55c1304cfeb478229a918aeda
Idan Arye <IdanArye@gmail.com>
2011-10-27 01:14:27 +0200

    Replaced the .BAT mechanism with submodules.

--- pullPlugins.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-CALL gitPullPlugin.bat git://github.com/vim-scripts dbext.vim
-CALL gitPullPlugin.bat git://github.com/tpope vim-fugitive
-CALL gitPullPlugin.bat git://repo.or.cz vcscommand .git
-CALL gitPullPlugin.bat git://github.com/gabemc powershell-vim
-CALL gitPullPlugin.bat git://github.com/tpope vim-unimpaired.git
-CALL gitPullPlugin.bat git://github.com/tpope vim-repeat.git
