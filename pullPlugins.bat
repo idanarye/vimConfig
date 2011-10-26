4341ea6112c2a85f7a1bd40d8388d1fb185326b6
Idan Arye <IdanArye@gmail.com>
2011-10-26 23:32:22 +0200

    Added vim-repeat

--- pullPlugins.bat
+++ pullPlugins.bat
@@ -5,3 +5,4 @@ CALL gitPullPlugin.bat git://github.com/tpope vim-fugitive
 CALL gitPullPlugin.bat git://repo.or.cz vcscommand .git
 CALL gitPullPlugin.bat git://github.com/gabemc powershell-vim
 CALL gitPullPlugin.bat git://github.com/tpope vim-unimpaired.git
+CALL gitPullPlugin.bat git://github.com/tpope vim-repeat.git
