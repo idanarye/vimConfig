cb44d6e9a68839092434fd0fef3b63196bc98acf
IdanArye <idanarye@gmail.com>
2017-12-01 03:21:01 +0200

    Update plugins

--- 0my-configurations/omnipytent/idan.py
+++ 0my-configurations/omnipytent/idan.py
@@ -5,3 +5,18 @@ python2 = local['python2']
 python3 = local['python3']
 cargo = local['cargo']
 dub = local['dub']
+
+
+def zip_vim_plugin(plugin_name):
+    import re
+
+    def version_numbers():
+        pattern = re.compile(r'^v?(\d+\.)\d+')
+        for tag in local['git']['tag']().splitlines():
+            if pattern.match(tag):
+                yield tuple(map(int, tag.strip('v').split('.')))
+
+    version_number = '.'.join(map(str, sorted(version_numbers())[-1]))
+    zip_command = local['zip']['%s_v%s.zip' % (plugin_name, version_number)]
+    zip_command = zip_command[local['git']['ls-files']().splitlines()]
+    return zip_command
