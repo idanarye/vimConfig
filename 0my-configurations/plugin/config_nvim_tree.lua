8d935dcbe901568dd117647a5c409faf8bf1dfbd
Idan Arye <idanarye@weka.io>
2021-10-31 12:49:18 +0200

    Move nvim-tree's dotfile hidding to Lua
    Also move all the Vim options to the Lua file.

--- 0my-configurations/plugin/config_nvim_tree.lua
+++ 0my-configurations/plugin/config_nvim_tree.lua
@@ -1,5 +1,29 @@
+vim.api.nvim_set_keymap('n', '<Leader><Tab>', ':NvimTreeToggle<Cr>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Leader><Leader><Tab>', ':NvimTreeFindFile<Cr>', {noremap = true, silent = true})
+
 require'nvim-tree'.setup {
     diagnostics = {
         enable = true;
+    };
+
+    filters = {
+        dotfiles = true;
+        custom = {};
+    };
+}
+
+-- These settings are here for now until the author of nvim-tree moves them to the setup function:
+vim.g.nvim_tree_gitignore = 1
+vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_highlight_opened_files = 1
+
+vim.g.nvim_tree_window_picker_exclude = {
+    filetype = {
+        'packer',
+        'qf'
+    };
+    buftype = {
+        'terminal'
     };
 }
