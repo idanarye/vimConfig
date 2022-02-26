ae78d158020470e18009315e09237e2d57388fc6
IdanArye <idanarye@gmail.com>
2022-02-26 02:35:40 +0200

    Disable treesitter indentation. It sucks

--- 0my-configurations/plugin/config_treesitter.lua
+++ 0my-configurations/plugin/config_treesitter.lua
@@ -2,5 +2,5 @@ require'nvim-treesitter.configs'.setup {
     highlight = { enable = true },
     incremental_selection = { enable = true },
     textobjects = { enable = true },
-    indent = { enable = true },
+    --indent = { enable = true },
 }
