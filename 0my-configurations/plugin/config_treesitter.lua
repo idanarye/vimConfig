2454461ed627a86423ef4a57460be68cd4e46586
Idan Arye <idanarye@weka.io>
2022-02-27 19:00:26 +0200

    Disable TreeSitter indent entirely. It sucks for Python too...

--- 0my-configurations/plugin/config_treesitter.lua
+++ 0my-configurations/plugin/config_treesitter.lua
@@ -2,8 +2,8 @@ require'nvim-treesitter.configs'.setup {
     highlight = { enable = true },
     incremental_selection = { enable = true },
     textobjects = { enable = true },
-    indent = {
-        enable = true,
-        disable = { 'rust' }, -- I only want this for Python
-    },
+    --indent = {
+        --enable = true,
+        --disable = { 'rust' }, -- I only want this for Python
+    --},
 }
