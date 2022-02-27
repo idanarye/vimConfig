00a27705d3c7cdc10a0700a1fa8e297a66e9ad02
Idan Arye <idanarye@weka.io>
2022-02-27 18:53:20 +0200

    Treesitter enable indent but not for Rust

--- 0my-configurations/plugin/config_treesitter.lua
+++ 0my-configurations/plugin/config_treesitter.lua
@@ -2,5 +2,8 @@ require'nvim-treesitter.configs'.setup {
     highlight = { enable = true },
     incremental_selection = { enable = true },
     textobjects = { enable = true },
-    --indent = { enable = true },
+    indent = {
+        enable = true,
+        disable = { 'rust' }, -- I only want this for Python
+    },
 }
