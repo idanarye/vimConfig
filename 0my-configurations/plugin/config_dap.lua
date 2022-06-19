fd7206403a860149c38b5ac5b1885d7b5b825df2
Idan Arye <idanarye@weka.io>
2022-06-19 13:07:40 +0300

    Update `config_dap.lua`

--- 0my-configurations/plugin/config_dap.lua
+++ 0my-configurations/plugin/config_dap.lua
@@ -1,8 +1,9 @@
 local dap = require'dap'
+local dapui = require'dapui'
 
-require'dapui'.setup({
-    sidebar = { open_on_start = false };
-    tray = { open_on_start = false };
+dapui.setup({
+    --sidebar = { open_on_start = false };
+    --tray = { open_on_start = false };
 })
 
 vim.g.dap_virtual_text = true
@@ -56,3 +57,18 @@ dap.configurations.cpp = {
 
 dap.configurations.c = dap.configurations.cpp
 dap.configurations.rust = dap.configurations.cpp
+
+vim.keymap.set('n', '<M-d>u', dapui.toggle)
+
+vim.keymap.set('n', '<M-d>c', dap.continue)
+vim.keymap.set('n', '<M-d>C', dap.run_to_cursor)
+vim.keymap.set('n', '<M-d>k', dap.close)
+vim.keymap.set('n', '<M-d>b', dap.toggle_breakpoint)
+vim.keymap.set('n', '<M-d><C-b>', function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
+vim.keymap.set('n', '<M-d>B', dap.clear_breakpoints)
+
+vim.keymap.set('n', '<M-d>o', dap.step_over)
+vim.keymap.set('n', '<M-d>i', dap.step_into)
+vim.keymap.set('n', '<M-d>O', dap.step_out)
+
+vim.keymap.set({'n', 'v'}, '<M-d>e', dapui.eval)
