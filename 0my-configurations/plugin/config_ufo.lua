cdd8a7c01ccc821dad0c2267f0da565784e21902
Idan Arye <idanarye@weka.io>
2022-08-16 20:13:59 +0300

    Configure ufo to use treesitter

--- 0my-configurations/plugin/config_ufo.lua
+++ 0my-configurations/plugin/config_ufo.lua
@@ -9,4 +9,8 @@ vim.o.foldenable = true
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
-require('ufo').setup()
+require('ufo').setup {
+    provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+    end
+}
