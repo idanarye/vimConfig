93ed861ea4fdaa3575536f73637c9435bbca8e1c
Idan Arye <IdanArye@gmail.com>
2022-12-01 16:33:38 +0200

    Make `generate_docs` reuse the window

--- 0my-configurations/lua/idan.lua
+++ 0my-configurations/lua/idan.lua
@@ -22,9 +22,23 @@ function M.run_block_in_ipython(job, block)
 end
 
 function M.generate_docs(cmd)
-    vim.cmd'botright vnew'
+    local existing_win = nil
+    for _, winid in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+        local bufnr = vim.api.nvim_win_get_buf(winid)
+        local found, value = pcall(vim.api.nvim_buf_get_var, bufnr, 'is_lemmy_docs_output')
+        if found and value then
+            existing_win = winid
+        end
+    end
+    if existing_win then
+        vim.fn.win_gotoid(existing_win)
+        vim.cmd.enew()
+    else
+        vim.cmd'botright vnew'
+    end
     local j = require'channelot'.terminal_job(cmd or 'make -s docs')
     vim.cmd.setfiletype('help')
+    vim.b.is_lemmy_docs_output = true
     vim.keymap.set('n', 'q', vim.cmd.quit, {buffer = true})
     j:wait()
     vim.cmd.checktime()
