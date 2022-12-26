db31a1728b71ba34f807b47f3c4de41748a81497
IdanArye <idanarye@gmail.com>
2022-12-26 23:02:03 +0200

    Use a Moonicipal file extending function for lua plugins

---
+++ 0my-configurations/lua/idan-lua-plugin.lua
@@ -0,0 +1,41 @@
+return function(T)
+    function T:choose_test()
+        local cc = self:cached_choice{key = tostring}
+        for filename, filetype in vim.fs.dir('tests') do
+            if filetype == 'file' and vim.endswith(filename, '_spec.lua') then
+                cc(filename)
+            end
+        end
+        return cc:select()
+    end
+
+    function T:test()
+        local testfile = T:choose_test()
+        vim.cmd'botright new'
+        vim.cmd.wincmd('20_')
+        vim.fn.termopen{
+            'nvim', '--headless',
+            '-u', 'tests/minimal_init.lua',
+            '-c', "PlenaryBustedDirectory tests/" .. testfile .. " {minimal_init = 'tests/minimal_init.lua'}",
+        }
+        vim.cmd.startinsert()
+    end
+
+    function T:test_all()
+        vim.cmd'botright new'
+        vim.cmd.wincmd('20_')
+        vim.fn.termopen{
+            'nvim', '--headless',
+            '-u', 'tests/minimal_init.lua',
+            '-c', "PlenaryBustedDirectory tests/ {minimal_init = 'tests/minimal_init.lua'}",
+        }
+        vim.cmd.startinsert()
+    end
+
+    function T:doc()
+        vim.fn.system('make docs')
+        vim.cmd.checktime()
+    end
+
+    return T
+end
