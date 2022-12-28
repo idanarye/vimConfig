617f4a83a3c433b2c5a3f2d62f90b8a0b447028e
Idan Arye <idanarye@weka.io>
2022-12-28 16:49:37 +0200

    Migrate to lazy.nvim

---
+++ init.lua
@@ -0,0 +1,39 @@
+local vim_config_path = vim.fs.dirname(debug.getinfo(1, 'S').short_src)
+vim.env.MYVIMRC = vim_config_path .. '/_vimrc'
+vim.api.nvim_command('source $MYVIMRC')
+
+local localcfg_fn = loadfile(vim.fs.dirname(vim_config_path) .. '/localcfg.lua')
+local localcfg
+if localcfg_fn then
+  localcfg = localcfg_fn()
+else
+  localcfg = {}
+end
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.runtimepath:prepend(lazypath)
+require'lazy'.setup(require'plugins', {
+  install = {
+    colorscheme = {'torte'},
+  },
+  performance = {
+    rtp = {
+      paths = {
+         vim_config_path .. '/0my-configurations',
+         unpack(localcfg.runtime_paths or {}),
+      },
+    },
+  }
+})
+
+vim.cmd.colorscheme('tortus')
