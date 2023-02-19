387218d19559d9eb99f46a5c20a9c80dcf20748a
Idan Arye <idanarye@weka.io>
2023-02-19 15:32:12 +0200

    Remove usage of `nvim-surround.config.termcode`

--- 0my-configurations/plugin/config_surround.lua
+++ 0my-configurations/plugin/config_surround.lua
@@ -1,8 +1,9 @@
-local config = require'nvim-surround.config'
+--local config = require'nvim-surround.config'
 require'nvim-surround'.setup {
     surrounds = {
         --['\29'] = {
-        [config.termcode'<C-]>'] = {
+        --[config.termcode'<C-]>'] = {
+        ['<C-]>'] = {
             add = { '{{', '}}' },
             --find = function()
                 --return M.get_selection({ motion = "a)" })
