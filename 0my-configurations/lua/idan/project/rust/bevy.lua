734abda5c13af843744a154f5c3c203247db1ef7
IdanArye <idanarye@gmail.com>
2023-03-09 22:41:13 +0200

    Fix Bevy dynamic linking parameter

--- 0my-configurations/lua/idan/project/rust/bevy.lua
+++ 0my-configurations/lua/idan/project/rust/bevy.lua
@@ -1,11 +1,12 @@
 ---@class IdanProjectRustBevyCfg : IdanProjectRustCfg
 
 ---@param cfg? IdanProjectRustBevyCfg
+---@param cfg? IdanProjectRustBevyCfg
 return function(T, cfg)
     cfg = cfg or {}
     if not cfg.extra_features_for_build_and_run then
         cfg.extra_features_for_build_and_run = {}
     end
-    table.insert(cfg.extra_features_for_build_and_run, 'bevy/dynamic')
+    table.insert(cfg.extra_features_for_build_and_run, 'bevy/dynamic_linking')
     return require'idan.project.rust'(T, cfg)
 end
