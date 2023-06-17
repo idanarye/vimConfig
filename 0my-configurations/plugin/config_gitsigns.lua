749a51b6419d2cb99c5bc8b636654123037c753e
IdanArye <IdanArye@gmail.com>
2023-06-17 21:02:49 +0300

    Fix gitsigns configurations

--- 0my-configurations/plugin/config_gitsigns.lua
+++ 0my-configurations/plugin/config_gitsigns.lua
@@ -1,23 +1,48 @@
-require('gitsigns').setup {
-    keymaps = {
-        -- Default keymap options
-        noremap = true,
-        buffer = true,
+local gs = require'gitsigns.actions'
+local ck = require'caskey'
 
-        ['n ]g'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
-        ['n [g'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
+require'gitsigns'.setup {
+    on_attach = function (bufnr)
+        ck.emit("Gitsigns", bufnr)
+    end,
+}
 
-        ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-        ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-        ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-        ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-        ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-        ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-        ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-        ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
+ck.setup {
+    mode = 'n',
+    name = 'Gitsigns',
+    when = ck.emitted'Gitsigns',
 
-        -- Text objects
-        ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-        ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
-    },
+    -- TODO: do I need these two?
+    -- ['n ]g'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
+    -- ['n [g'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
+
+    ['<leader>hs'] = { act = gs.stage_hunk, desc = 'stage hunk' },
+    ['<leader>hu'] = { act = gs.undo_stage_hunk, desc = 'undo stage hunk' },
+    ['<leader>hr'] = { act = gs.reset_hunk, desc = 'reset hunk' },
+
+    ['<leader>hR'] = { act = gs.reset_buffer, desc = 'reset buffer' },
+    ['<leader>hp'] = { act = gs.preview_hunk, desc = 'preview hunk' },
+    ['<leader>hb'] = { act = function() gs.blame_line(true) end, desc = 'blame line' },
+}
+
+ck.setup {
+    mode = 'v',
+    name = 'Gitsigns',
+    when = ck.emitted'Gitsigns',
+
+    ['<leader>hs'] = { act = function()
+        gs.stage_hunk({vim.fn.line('.'), vim.fn.line('v')})
+    end, desc = 'stage hunk' },
+
+    ['<leader>hr'] = { act = function()
+        gs.reset_hunk({vim.fn.line('.'), vim.fn.line('v')})
+    end, desc = 'reset hunk' },
+}
+
+-- Text objects
+ck.setup {
+    mode = {'o', 'x'},
+    name = 'Gitsigns',
+    when = ck.emitted'Gitsigns',
+    ['ih'] = { act = gs.select_hunk, desc = 'select hunk' },
 }
