a71736955993d47025721a2c694248551ff66310
Idan Arye <idanarye@weka.io>
2023-08-24 19:09:24 +0300

    Make Octo use fzf-lua

--- 0my-configurations/plugin/config_octo.lua
+++ 0my-configurations/plugin/config_octo.lua
@@ -1,2 +1,10 @@
 require'octo'.setup {
+    picker = 'fzf-lua',
+}
+
+local ck = require'caskey'
+ck.setup {
+    mode = {'n'},
+    name = 'Octo',
+    ['<Leader>go'] = { act = ck.cmd'Octo actions' },
 }
