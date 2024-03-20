ebef566faf04471dc341ccb9ee5e3ca008171dfb
Idan Arye <idanarye@weka.io>
2024-03-20 19:57:53 +0200

    Add nvim-days-without (dormant for now)

--- 0my-configurations/lua/plugins/my_plugins.lua
+++ 0my-configurations/lua/plugins/my_plugins.lua
@@ -15,6 +15,6 @@ return {
     {'idanarye/nvim-moonicipal', branch = 'develop'},
     {'idanarye/nvim-channelot', branch = 'develop'},
     {'idanarye/nvim-buffls', branch = 'develop'},
-
     {'idanarye/nvim-blunder', branch = 'develop'},
+    {'idanarye/nvim-days-without', branch = 'develop'},
 }
