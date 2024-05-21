3c59e5f927e8268c5a7d38b91f47d2fad7933bc5
Idan Arye <idanarye@weka.io>
2024-05-21 16:58:59 +0300

    Add some more stuff from unimpaired

--- 0my-configurations/plugin/config_impairative.lua
+++ 0my-configurations/plugin/config_impairative.lua
@@ -92,3 +92,74 @@ impairative.toggling {
         vim.o.cursorcolumn = value
     end
 }
+
+impairative.operations {
+    -- Use capital O for now to avoid conflicts
+    backward = '<Leader>[',
+    forward = '<Leader>]',
+}
+:command_pair {
+    key = 'a',
+    backward = 'previous',
+    forward = 'next',
+}
+:command_pair {
+    key = 'A',
+    backward = 'first',
+    forward = 'last',
+}
+:command_pair {
+    key = 'b',
+    backward = 'bprevious',
+    forward = 'bnext',
+}
+:command_pair {
+    key = 'B',
+    backward = 'bfirst',
+    forward = 'blast',
+}
+:command_pair {
+    key = 'l',
+    backward = 'lprevious',
+    forward = 'lnext',
+}
+:command_pair {
+    key = 'L',
+    backward = 'lfirst',
+    forward = 'llast',
+}
+:command_pair {
+    key = '<C-l>',
+    backward = 'lpfile',
+    forward = 'lnfile',
+}
+:command_pair {
+    key = 'q',
+    backward = 'cprevious',
+    forward = 'cnext',
+}
+:command_pair {
+    key = 'Q',
+    backward = 'cfirst',
+    forward = 'clast',
+}
+:command_pair {
+    key = '<C-q>',
+    backward = 'cpfile',
+    forward = 'cnfile',
+}
+:command_pair {
+    key = 't',
+    backward = 'tprevious',
+    forward = 'tnext',
+}
+:command_pair {
+    key = 'T',
+    backward = 'tfirst',
+    forward = 'tlast',
+}
+:command_pair {
+    key = '<C-t>',
+    backward = 'ptprevious',
+    forward = 'ptnext',
+}
