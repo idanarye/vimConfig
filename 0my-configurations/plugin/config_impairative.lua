4f2555b6561563843ec1651b31e88103ecc3835f
Idan Arye <IdanArye@gmail.com>
2024-05-23 19:41:20 +0300

    Use Impairative to toggle `conceallevel`

--- 0my-configurations/plugin/config_impairative.lua
+++ 0my-configurations/plugin/config_impairative.lua
@@ -1,2 +1,14 @@
---local impairative = require'impairative'
 require'impairative.replicate-unimpaired'()
+
+local impairative = require'impairative'
+
+impairative.toggling {
+    disable = ']o',
+    enable = '[o',
+    toggle = 'yo',
+}
+:option {
+    key = 'e',
+    option = 'conceallevel',
+    values = {[true] = 2, [false] = 0},
+}
