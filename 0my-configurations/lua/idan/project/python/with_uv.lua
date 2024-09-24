404f375cd9a70d94619cb190c29945f778fdd8e7
Idan Arye <IdanArye@gmail.com>
2024-09-24 14:41:56 +0300

    Add uv project for Python Moonicipal templates

---
+++ 0my-configurations/lua/idan/project/python/with_uv.lua
@@ -0,0 +1,35 @@
+local moonicipal = require'moonicipal'
+local blunder = require'blunder'
+local channelot = require'channelot'
+
+---@class IdanWorkProjectPythonWithUvCfg
+---@field entry_point? string
+
+---@param cfg? IdanWorkProjectPythonWithUvCfg
+return function(cfg)
+    cfg = cfg or {}
+
+    local T = moonicipal.tasks_lib()
+
+    function T:entry_point()
+        return cfg.entry_point or 'main.py'
+    end
+
+    function T:run()
+        blunder.run{'uv', '-q', '--isolated', 'run', T:entry_point()}
+    end
+
+    function T:debug()
+        require'dap'.run {
+            type = 'python',
+            request = 'launch',
+            program = T:entry_point(),
+        }
+    end
+
+    function T:explore()
+        channelot.windowed_terminal_job{'uv', '-q', '--isolated', 'run', 'ipython', '-i', T:entry_point()}
+    end
+
+    return T
+end
