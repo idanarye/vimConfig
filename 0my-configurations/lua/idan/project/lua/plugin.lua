38551d76b6d2a7be45547d89fffe6027d1383588
Idan Arye <IdanArye@gmail.com>
2024-10-11 02:46:20 +0300

    Make the Lua plugin project for Moonicipal use the new `import` mechanism

--- 0my-configurations/lua/idan/project/lua/plugin.lua
+++ 0my-configurations/lua/idan/project/lua/plugin.lua
@@ -1,44 +1,46 @@
 local moonicipal = require'moonicipal'
 local blunder = require'blunder'
 
-local T = moonicipal.tasks_lib()
+return function()
+    local T = moonicipal.tasks_lib()
 
-function T:choose_test()
-    local cc = self:cached_choice{key = tostring}
-    for filename, filetype in vim.fs.dir('tests') do
-        if filetype == 'file' and vim.endswith(filename, '_spec.lua') then
-            cc(filename)
+    function T:choose_test()
+        local cc = self:cached_choice{key = tostring}
+        for filename, filetype in vim.fs.dir('tests') do
+            if filetype == 'file' and vim.endswith(filename, '_spec.lua') then
+                cc(filename)
+            end
         end
+        return cc:select()
     end
-    return cc:select()
-end
 
-function T:test()
-    local testfile = T:choose_test()
-    blunder.create_window_for_terminal()
-    vim.cmd.wincmd('20_')
-    vim.fn.termopen{
-        'nvim', '--headless',
-        '-u', 'tests/minimal_init.lua',
-        '-c', "PlenaryBustedDirectory tests/" .. testfile .. " {minimal_init = 'tests/minimal_init.lua'}",
-    }
-    vim.cmd.startinsert()
-end
+    function T:test()
+        local testfile = T:choose_test()
+        blunder.create_window_for_terminal()
+        vim.cmd.wincmd('20_')
+        vim.fn.termopen{
+            'nvim', '--headless',
+            '-u', 'tests/minimal_init.lua',
+            '-c', "PlenaryBustedDirectory tests/" .. testfile .. " {minimal_init = 'tests/minimal_init.lua'}",
+        }
+        vim.cmd.startinsert()
+    end
 
-function T:test_all()
-    blunder.create_window_for_terminal()
-    vim.cmd.wincmd('20_')
-    vim.fn.termopen{
-        'nvim', '--headless',
-        '-u', 'tests/minimal_init.lua',
-        '-c', "PlenaryBustedDirectory tests/ {minimal_init = 'tests/minimal_init.lua'}",
-    }
-    vim.cmd.startinsert()
-end
+    function T:test_all()
+        blunder.create_window_for_terminal()
+        vim.cmd.wincmd('20_')
+        vim.fn.termopen{
+            'nvim', '--headless',
+            '-u', 'tests/minimal_init.lua',
+            '-c', "PlenaryBustedDirectory tests/ {minimal_init = 'tests/minimal_init.lua'}",
+        }
+        vim.cmd.startinsert()
+    end
 
-function T:doc()
-    vim.fn.system('make docs')
-    vim.cmd.checktime()
-end
+    function T:doc()
+        vim.fn.system('make docs')
+        vim.cmd.checktime()
+    end
 
-return T
+    return T
+end
