ce6da560c22b16f94a5c8233dfb92665ed183536
Idan Arye <IdanArye@gmail.com>
2024-10-31 16:38:09 +0200

    Fix rust-analyzer configuration in Rustaceanvim config

--- 0my-configurations/plugin/config_rust.lua
+++ 0my-configurations/plugin/config_rust.lua
@@ -1,23 +1,22 @@
 vim.g.rustaceanvim = {
-    tools = {
-        server = {
-            default_settings = {
-                ["rust-analyzer"] = {
-                    assist = {
-                        importGranularity = "module";
-                        importPrefix = "by_self";
-                    },
-                    cargo = {
-                        loadOutDirsFromCheck = true;
-                        features = 'all';
-                    },
-                    procMacro = {
-                        enable = true;
-                    },
-                    completion = {
-                        -- addCallArgumentSnippets = true;
-                        -- addCallParenthesis = true;
-                    },
+    server = {
+        -- capabilities = vim.lsp.protocol.make_client_capabilities(),
+        default_settings = {
+            ["rust-analyzer"] = {
+                assist = {
+                    importGranularity = "module",
+                    importPrefix = "by_self",
+                },
+                cargo = {
+                    loadOutDirsFromCheck = true,
+                    -- features = 'all',
+                },
+                procMacro = {
+                    enable = true,
+                },
+                completion = {
+                    addCallArgumentSnippets = true,
+                    -- addCallParenthesis = true,
                 },
             },
         },
