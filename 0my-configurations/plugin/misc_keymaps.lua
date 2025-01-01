aef6e0ee2fbcdc7b3584117b1cb0dc0c9c108340
Idan Arye <IdanArye@gmail.com>
2025-01-01 22:27:55 +0200

    Add a keymap to close all bubble windows

--- 0my-configurations/plugin/misc_keymaps.lua
+++ 0my-configurations/plugin/misc_keymaps.lua
@@ -8,5 +8,13 @@ ck.setup {
             vim.fn.termopen{'nu'}
             vim.cmd.startinsert()
         end, desc = 'Start Terminal' },
-    }
+    },
+    ['<C-w>u'] = { act = function()
+        for _, winnr in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+            local win_config = vim.api.nvim_win_get_config(winnr)
+            if win_config.relative and win_config.relative ~= '' then
+                vim.api.nvim_win_close(winnr, false)
+            end
+        end
+    end, desc = 'Close all bubble windows'},
 }
