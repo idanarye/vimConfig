b546f7c60e4c713ae6d26debfede9315e311e0b1
Idan Arye <IdanArye@gmail.com>
2025-05-04 17:14:51 +0300

    Change mode when debugmaster is active

--- 0my-configurations/plugin/config_lualine.lua
+++ 0my-configurations/plugin/config_lualine.lua
@@ -1,4 +1,29 @@
+require'debugmaster.debug.mode'
+local function replace_if_debugmaster_active(value)
+    return function(...)
+        local mode = vim.api.nvim_get_mode()
+        if mode.mode ~= 'n' then
+            return ...
+        end
+        if require'debugmaster.debug.mode'.is_active() then
+            return value
+        else
+            return ...
+        end
+    end
+end
+
 require'lualine'.setup {
+    sections = {
+        lualine_a = {
+            {
+                'mode',
+                fmt = replace_if_debugmaster_active('DEBUG'),
+                -- This doesn't work properly
+                -- color = replace_if_debugmaster_active('dCursor'),
+            },
+        },
+    },
     inactive_sections = {
         lualine_a = {
             function()
@@ -7,3 +32,5 @@ require'lualine'.setup {
         },
     },
 }
+
+require'lualine'.get_config().sections.lualine_a = {}
