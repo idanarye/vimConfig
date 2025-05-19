dd5013972af3e04f088afff2c2b74599871a45ac
Idan Arye <IdanArye@gmail.com>
2025-05-19 16:44:58 +0300

    Improve Quicker configuration

--- 0my-configurations/plugin/config_quickfix.lua
+++ 0my-configurations/plugin/config_quickfix.lua
@@ -5,6 +5,23 @@ quicker.setup {
     edit = {
         enabled = false,
     },
+    constrain_cursor = false,
+    keys = {
+        {
+            '>',
+            function()
+                require('quicker').expand({ before = 2, after = 2, add_to_existing = true })
+            end,
+            desc = 'Expand quickfix context',
+        },
+        {
+            '<',
+            function()
+                require('quicker').collapse()
+            end,
+            desc = 'Collapse quickfix context',
+        },
+    },
 }
 
 ck.setup {
