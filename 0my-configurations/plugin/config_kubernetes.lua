86c91fcd4c440d89d5dbbc901e3d78503ce419db
Idan Arye <IdanArye@gmail.com>
2025-11-05 17:47:14 +0200

    Make `<Leader>k` a leader for Kubectl.nvim commands

--- 0my-configurations/plugin/config_kubernetes.lua
+++ 0my-configurations/plugin/config_kubernetes.lua
@@ -2,33 +2,7 @@ local kubectl = require'kubectl'
 kubectl.setup {
 }
 
-require'caskey'.setup {
-    mode = {'n'},
-    name = 'kubectl',
-    ['<Leader>k'] = {
-        act = function()
-            if not vim.env.KUBECONFIG then
-                vim.notify('$KUBECONFIG is not set. Please run the :KubeSelectConfigFile command', vim.log.levels.WARN)
-                return
-            end
-            local known_resource_types = vim.tbl_keys(require'kubectl.cache'.cached_api_resources.values)
-            if next(known_resource_types) == nil then
-                kubectl.open()
-            else
-                require'moonicipal.util'.defer_to_coroutine(function()
-                    local resource_type = require'moonicipal'.select(known_resource_types, {
-                    })
-                    if resource_type then
-                        vim.cmd.Kubectl('get', resource_type)
-                    end
-                end)
-            end
-        end,
-        desc = 'Toggle kubectl.nvim',
-    }
-}
-
-vim.api.nvim_create_user_command('KubeSelectConfigFile', function()
+local function select_kubeconfig_file()
     require'fzf-lua'.fzf_exec(function(cb)
         local pattern = vim.regex[=[^kube.*.yaml$]=]
         for path, typ in vim.fs.dir('~') do
@@ -40,15 +14,53 @@ vim.api.nvim_create_user_command('KubeSelectConfigFile', function()
     end,
     {
         preview = function(path)
-            return vim.system({'nu', '-c', [=[ls $env.INPUT | first]=]
-        }, {
-            env = {INPUT = path[1]},
-        }):wait().stdout
-    end,
-    actions = {
-        default = function(path)
-            vim.env.KUBECONFIG = path[1]
+            return vim.system({'nu', '-c', [=[ls $env.INPUT | first]=]}, {
+                env = {INPUT = path[1]},
+            }):wait().stdout
         end,
-    },
-})
-end, {})
+        actions = {
+            default = function(path)
+                vim.env.KUBECONFIG = path[1]
+            end,
+        },
+    })
+end
+
+vim.api.nvim_create_user_command('KubeSelectConfigFile', select_kubeconfig_file, {})
+
+require'caskey'.setup {
+    mode = {'n'},
+    name = 'kubectl',
+    ['<Leader>k'] = {
+        ['o'] = {
+            act = function()
+                if vim.env.KUBECONFIG then
+                    kubectl.open()
+                else
+                    vim.notify('$KUBECONFIG is not set. Please run the :KubeSelectConfigFile command', vim.log.levels.WARN)
+                end
+            end,
+            desc = 'Open kubectl.nvim',
+        },
+        ['r'] = {
+            act = function()
+                local known_resource_types = vim.tbl_keys(require'kubectl.cache'.cached_api_resources.values)
+                if next(known_resource_types) == nil then
+                    return
+                end
+                require'moonicipal.util'.defer_to_coroutine(function()
+                    local resource_type = require'moonicipal'.select(known_resource_types, {
+                    })
+                    if resource_type then
+                        vim.cmd.Kubectl('get', resource_type)
+                    end
+                end)
+            end,
+            desc = 'Choose Kubernetes resource',
+        },
+        ['c'] = {
+            act = select_kubeconfig_file,
+            desc = 'Choose KUBECONFIG file',
+        }
+    }
+}
