55fffb0d8f97d34594b67f7f6ec6d34200bcf77d
Idan Arye <IdanArye@gmail.com>
2026-01-05 18:43:44 +0200

    Add `shell_workflows.lua` (mainly keymaps)

---
+++ 0my-configurations/plugin/shell_workflows.lua
@@ -0,0 +1,72 @@
+local ck = require'caskey'
+
+local augroup = vim.api.nvim_create_augroup('idan:shell_workflows', {})
+
+local window_visit_counter = 0
+local window_visit_order_varname = '-window-visit-order-'
+
+vim.api.nvim_create_autocmd('WinEnter', {
+    group = augroup,
+    callback = function()
+        window_visit_counter = window_visit_counter + 1
+        vim.api.nvim_win_set_var(0, window_visit_order_varname, window_visit_counter)
+    end,
+})
+
+local function is_shell_like(win_id)
+    local buf = vim.api.nvim_win_get_buf(win_id or 0)
+    local buftype = vim.api.nvim_get_option_value('buftype', {buf = buf})
+    return buftype == 'terminal' or buftype == 'prompt'
+end
+
+---@param should_be_shell boolean
+local function last_accessed_window_of_type(should_be_shell)
+    local latest_win = nil
+    local max_order = nil
+    local current_win = vim.api.nvim_get_current_win()
+    for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+        if win ~= current_win and is_shell_like(win) == should_be_shell then
+            local ok, order = pcall(vim.api.nvim_win_get_var, win, window_visit_order_varname)
+            if not ok then
+                order = 0
+            end
+            if max_order == nil or max_order < order then
+                latest_win = win
+                max_order = order
+            end
+        end
+    end
+    return latest_win
+end
+
+ck.setup {
+    mode = {'n', 'v', 'i', 't'},
+    ['<C-\\>'] = {
+        ['<C-s>'] = { act = function()
+            require'channelot'.create_window_for_terminal()
+            vim.fn.jobstart({'nu'}, {term = true})
+        end, desc = 'Start Terminal'},
+        ['<C-e>'] = { act = function()
+            if is_shell_like(0) then
+                vim.cmd.startinsert()
+                return
+            end
+            local last_win = last_accessed_window_of_type(true)
+            if last_win == nil then
+                return
+            end                  
+            vim.api.nvim_set_current_win(last_win)
+            vim.cmd.startinsert()
+        end, desc = 'Go to last shell'},
+        ['<C-w>'] = { act = function()
+            if not is_shell_like(0) then
+                return
+            end
+            local last_win = last_accessed_window_of_type(false)
+            if last_win == nil then
+                return
+            end
+            vim.api.nvim_set_current_win(last_win)
+        end, desc = 'Go to last non-shell'},
+    },
+}
