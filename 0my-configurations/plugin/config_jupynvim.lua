8641c56a4f7d19097f7ae507c56210d50c6e4e18
Idan Arye <IdanArye@gmail.com>
2026-05-12 13:00:31 +0300

    Don't let jupynvim modify conceal options

--- 0my-configurations/plugin/config_jupynvim.lua
+++ 0my-configurations/plugin/config_jupynvim.lua
@@ -1,2 +1,9 @@
+local orig_opts = vim.iter({'conceallevel', 'concealcursor'}):fold({}, function(acc, k)
+    acc[k] = vim.o[k]
+    return acc
+end)
 require'jupynvim'.setup {
 }
+for k, v in pairs(orig_opts) do
+    vim.o[k] = v
+end
